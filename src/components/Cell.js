23c0fef2ae179e24797ac3b62245d69e60f18382
Minh Bui <tranduyminh.bui@gmail.com>
2019-11-03 15:54:05 +1030

    finish styled

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -3,11 +3,7 @@ import {StyledCell} from "./styles/StyledCell";
 import {TETROMINOS} from "../tetrominos";
 
 function Cell({type}) {
-  return (
-    <StyledCell type={"L"} color={TETROMINOS["L"].color}>
-      cell
-    </StyledCell>
-  );
+  return <StyledCell type={"L"} color={TETROMINOS["L"].color}></StyledCell>;
 }
 
 export default Cell;
